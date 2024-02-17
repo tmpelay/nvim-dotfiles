454986ad1ed8b33e0f0ba3bdc227f15b645ae858
TMPela <tmpmalleret@gmail.com>
2024-02-17 17:17:30 -0300

    treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,38 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
- }
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects"
+  },
+  build = ":TSUpdate",
+  event = "VeryLazy",
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "luadoc",
+      "javascript",
+      "html",
+      "css",
+      "bash",
+      "cpp",
+      "python"
+    }, highlight = {
+      enable = true
+    }, indent = {
+      enable = true
+    }, textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@conditional.outer",
+          ["ic"] = "@conditional.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+        }
+      }
+    }
+  }
+}
