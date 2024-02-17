b9dcc92e86ace2febba1710558019899ebffd766
TMPela <tmpmalleret@gmail.com>
2024-02-17 16:41:12 -0300

    quit keymap

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,2 +1,5 @@
--- map leader+w to save current file in normal mode
-vim.keymap.set("n", "<C-s>", ":write<CR>", { noremap = true, silent = true })
+-- map Ctrl+s to save current file in normal mode
+vim.keymap.set("n", "<c-s>", ":write<cr>", { noremap = true, silent = true })
+
+-- map Ctrl+q to close current buffer in normal mode
+vim.keymap.set("n", "<c-q>", ":quit<cr>", { noremap = true, silent = true })
