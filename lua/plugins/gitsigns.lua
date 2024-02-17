3e52c2131a14fac887d599634550c6f29a513a8c
TMPela <tmpmalleret@gmail.com>
2024-02-17 16:47:33 -0300

    gitsigns better

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,16 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = true,
+  opts = {
+    numhl = true,
+    max_file_lenght = 10000,
+    signs = {
+      add = { text = '+' },
+      change = { text = '│' },
+      delete = { text = '-' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+      untracked = { text = '┆' },
+    }
+  }
 }
