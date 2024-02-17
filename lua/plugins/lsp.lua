70177b2a25155bf6cfaf3f8d341ec424dad659a1
TMPela <tmpmalleret@gmail.com>
2024-02-17 17:32:09 -0300

    lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,7 +12,19 @@ return {
 
     local on_attach = function(_, bufnr)
       vim.bo[bufnr].omnifunc = 'v:lua.vim.lps.omnifunc'
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = bufnr})
+      local opts = { buffer = bufnr }
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+      vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+      vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+      vim.keymap.set('n', '<space>f', function()
+        vim.lsp.buf.format { async = true }
+      end, opts)
     end
 
     require("neodev").setup()
@@ -27,5 +39,12 @@ return {
     })
     require("lspconfig").cmake.setup({})
     require("lspconfig").clangd.setup({})
+    require("lspconfig").cssls.setup({})
+    require("lspconfig").emmet_ls.setup({})
+    require("lspconfig").html.setup({})
+    require("lspconfig").java_language_server.setup({})
+    require("lspconfig").kotlin_language_server.setup({})
+    require("lspconfig").pyright.setup({})
+    require("lspconfig").tsserver.setup({})
   end
 }
