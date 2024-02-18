03070efd56aad5e884e40643612d4d71998e483a
TMPela <tmpmalleret@gmail.com>
2024-02-18 07:33:14 -0300

    ssh

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,6 +3,7 @@ return {
   cmd = {"G", "Git"},
   keys = {
     {"<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch"},
-    {"<leader>gl", ":Git pull<cr>", desc = "Git pull"}
+    {"<leader>gl", ":Git pull<cr>", desc = "Git pull"},
+    {"<leader>gp", ":Git push<cr>", desc = "Git push"}
   }
 }
